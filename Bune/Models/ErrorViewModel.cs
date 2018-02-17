e0ea0e037e67460a5eccffbaa5b5ce4d2068e01d
Your Name <r.wobben@home.nl>
2018-02-17 23:33:46 +0100

    first commit

---
+++ Bune/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bune.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
